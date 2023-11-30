a321609543ba204971dfaa28d18b90cf652ed7e5
alessandrorumi <rumi.alessandro21@gmail.com>
2023-11-30 14:33:44 +0100

    Add: Elementi html; Feature: Function num. random

---
+++ script/myScript.js
@@ -0,0 +1,46 @@
+// Visualizzare in pagina 5 numeri casuali.
+// Timer di 30 secondi. Dopo 30 secondi i numeri scompaiono e l’utente deve inserire i numeri.
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+// Dichiarazione numeri casuali
+const randomNumbers = document.getElementById('numbers');
+
+// Dichiarazione Start Timer
+const startTimer = document.getElementById('reminder');
+
+// Dichiarazione tasto genera
+const generateButton = document.getElementById('generate');
+
+// Al click del tasto genera
+generateButton.addEventListener('click', function() {
+
+  // Genera (5) numeri casuali (da 1 a 10)
+  const numbersToRemember = getRandomNumbers(1, 100, 5);
+
+  // Mostra in html
+  startTimer.innerHTML = `Hai 30 secondi per ricordarti questi numeri!`
+  randomNumbers.innerHTML = numbersToRemember;
+
+
+});
+
+
+
+
+// Funzioni
+
+// Funzione per generare numeri random
+function getRandomNumbers(min, max, arrayLength) {
+
+  let randomArray = [];
+
+  while (randomArray.length < arrayLength) {
+    const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    if(!randomArray.includes(randomNumber)) {
+      randomArray.push(randomNumber);
+    }
+  }  
+
+  return randomArray;
+}
